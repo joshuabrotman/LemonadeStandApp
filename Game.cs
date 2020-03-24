8e5c3603571b0d468a86d1dad4ff7d461abdc93d
magnetoplasmadynamics <link1922@gmail.com>
2020-03-23 19:51:56 -0500

    currently working on decrementing the items during a sale

--- Game.cs
+++ Game.cs
@@ -17,6 +17,12 @@ namespace LemonadeStand_3DayStarter
         public int temperature;
         public int humidity;
         public static ConsoleKeyInfo key;
+        public double drinkPrice;
+        public int sugarCubesPerCup;
+        public int iceCubesPerCup;
+        public double lemonsPerCup;
+        public int salesCount =0;
+        public int sale;
 
         public Store NewStore = new Store();
         public Player NewPlayer = new Player();
@@ -45,8 +51,11 @@ namespace LemonadeStand_3DayStarter
 
             //visit store/inventory
             BuyItems();
+            SetPrice();
+            SetRecipe();
             Console.Clear();
             DisplayBoard();
+            CustomerVisit();
 
             //control quality/recipe and price
 
@@ -69,9 +78,11 @@ namespace LemonadeStand_3DayStarter
                             for (int l = 0; l < 60; l++)
                             {
 
+                                UpdateInventory();
                                 DisplayWeather();
                                 UpdateTime();
                                 UpdateWallet();
+                                DisplayDrinkPrice();
 
                                 Thread.Sleep(0);
                                 seconds++;
@@ -96,6 +107,67 @@ namespace LemonadeStand_3DayStarter
 
 
         }
+
+        public void CustomerVisit()
+        {
+            if (MadeASale)
+            {
+                NewPlayer.inventory.lemons -= lemonsPerCup;
+                NewPlayer.inventory.sugarCubes -= sugarCubesPerCup;
+                NewPlayer.inventory.iceCubes -= iceCubesPerCup;
+                NewPlayer.inventory.cups.Decrement;
+            }
+        }
+
+        public bool MadeASale()
+        {
+            Random saleChance = new Random();
+            sale = saleChance.Next(0, 100);
+            if(sale >= 49) 
+            {
+                salesCount++;
+                Console.SetCursorPosition(14, 3);
+                Console.Write(salesCount);
+                return true; 
+            }
+            return false;
+        }
+
+        public void SetPrice()
+        {
+            Console.Clear();
+            Console.WriteLine("Please enter the price per cup: (00.25) ");
+            drinkPrice = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public void SetRecipe()
+        {
+            Console.Clear();
+            Console.WriteLine("Please enter the lemons per cup: (as a decimal) ");
+            lemonsPerCup = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter the sugar cubes per cup:");
+            sugarCubesPerCup = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the ice cubes per cup:");
+            iceCubesPerCup = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void DisplayDrinkPrice()
+        {
+            Console.SetCursorPosition(65, 2);
+            Console.Write(drinkPrice);
+        }
+
+        public void UpdateInventory()
+        {
+            Console.SetCursorPosition(1, 2);
+            Console.Write("Lemons: "+NewPlayer.inventory.lemons.Count());
+            Console.SetCursorPosition(1, 3);
+            Console.Write("Sugar: "+NewPlayer.inventory.sugarCubes.Count());
+            Console.SetCursorPosition(1, 4);
+            Console.Write("Ice: "+NewPlayer.inventory.iceCubes.Count());
+            Console.SetCursorPosition(1, 5);
+            Console.Write("Cups: "+NewPlayer.inventory.cups.Count());
+        }
         public void IsKeyPressed()
         {
             if (Console.KeyAvailable)
@@ -105,6 +177,18 @@ namespace LemonadeStand_3DayStarter
                 {
                     Console.Clear();
                     BuyItems();
+
+                }
+                if (Game.key.Key == ConsoleKey.R)
+                {
+                    Console.Clear();
+                    SetRecipe();
+
+                }
+                if (Game.key.Key == ConsoleKey.P)
+                {
+                    Console.Clear();
+                    SetPrice();
 
                 }
 
@@ -121,11 +205,11 @@ namespace LemonadeStand_3DayStarter
             humidity = random2.Next(40, 80);
             raining = random3.Next(0, 100);
 
-            Console.SetCursorPosition(37, 2);
-            Console.Write(temperature + " °F  ");
-            Console.SetCursorPosition(37, 3);
-            Console.Write(humidity + " %Humid  ");
-            Console.SetCursorPosition(37, 4);
+            Console.SetCursorPosition(38, 2);
+            Console.Write(temperature + "°F  ");
+            Console.SetCursorPosition(38, 3);
+            Console.Write(humidity + "% Humidity");
+            Console.SetCursorPosition(38, 4);
             if (raining > 49)
             {
                 Console.Write("Raining    ");
@@ -140,11 +224,11 @@ namespace LemonadeStand_3DayStarter
         public void DisplayWeather()
         {
             
-            Console.SetCursorPosition(37, 2);
-            Console.Write(temperature + " °F  ");
-            Console.SetCursorPosition(37, 3);
-            Console.Write(humidity + " %Humid  ");
-            Console.SetCursorPosition(37, 4);
+            Console.SetCursorPosition(40, 2);
+            Console.Write(temperature + "°F  ");
+            Console.SetCursorPosition(40, 3);
+            Console.Write(humidity + "% Humidity");
+            Console.SetCursorPosition(40, 4);
             if (raining > 49)
             {
                 Console.Write("Raining    ");
@@ -160,12 +244,12 @@ namespace LemonadeStand_3DayStarter
         public void DisplayBoard()
         {
             Console.SetCursorPosition(0, 0);
-            Console.Write("Inventory | Sales      | Time       | Weather   | Recipe   | Price | Wallet \n");
-            Console.Write("          |            |            |           |          |       |        \n");
-            Console.Write("          |            |            |           |          |       |        \n");
-            Console.Write("          |            |            |           |          |       |        \n");
-            Console.Write("          |            |            |           |          |       |        \n");
-            Console.Write("          |            |            |           |          |       |        \n");
+            Console.Write(" Inventory  | Sales      | Time       | Weather     | Recipe   | Price | Wallet \n");
+            Console.Write("            |            |            |             |          |       |        \n");
+            Console.Write("            |            |            |             |          |       |        \n");
+            Console.Write("            |            |            |             |          |       |        \n");
+            Console.Write("            |            |            |             |          |       |        \n");
+            Console.Write("            |            |            |             |          |       |        \n\n");
             Console.Write("Press B - Buy more - R - Change Recipe - P - Adjust Price    \n");
 
 
@@ -174,7 +258,7 @@ namespace LemonadeStand_3DayStarter
 
         public void UpdateWallet()
         {
-            Console.SetCursorPosition(69, 2);
+            Console.SetCursorPosition(73, 2);
             Console.Write("$"+NewPlayer.wallet.Money);
         }
 
@@ -183,39 +267,39 @@ namespace LemonadeStand_3DayStarter
             switch (days)
             {
                 case 0:
-                    Console.SetCursorPosition(25, 2);
+                    Console.SetCursorPosition(27, 2);
                     Console.Write("Monday   ");
                     break;
                 case 1:
-                    Console.SetCursorPosition(25, 2);
+                    Console.SetCursorPosition(27, 2);
                     Console.Write("Tuesday   ");
                     break;
                 case 2:
-                    Console.SetCursorPosition(25, 2);
+                    Console.SetCursorPosition(27, 2);
                     Console.Write("Wednesday  ");
                     break;
                 case 3:
-                    Console.SetCursorPosition(25, 2);
+                    Console.SetCursorPosition(27, 2);
                     Console.Write("Thursday  ");
                     break;
                 case 4:
-                    Console.SetCursorPosition(25, 2);
+                    Console.SetCursorPosition(27, 2);
                     Console.Write("Friday   ");
                     break;
                 case 5:
-                    Console.SetCursorPosition(25, 2);
+                    Console.SetCursorPosition(27, 2);
                     Console.Write("Saturday  ");
                     break;
                 case 6:
-                    Console.SetCursorPosition(25, 2);
+                    Console.SetCursorPosition(27, 2);
                     Console.Write("Sunday   ");
                     break;
 
             }
 
-            Console.SetCursorPosition(25, 3);
+            Console.SetCursorPosition(27, 3);
             Console.Write(hours + ":" + minutes + ":" + seconds);
-            Console.SetCursorPosition(25, 4);
+            Console.SetCursorPosition(27, 4);
             Console.Write("Days: " + days);
 
 
